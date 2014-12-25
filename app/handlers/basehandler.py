97424fec31699adfa59dd12af03eefa6e1c4c435
yukirin <standupdown@gmail.com>
2014-12-26 06:58:38 +0900

    Add Basehandler

---
+++ app/handlers/basehandler.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from tornado.web import RequestHandler
+
+
+class BaseHandler(RequestHandler):
+    def set_cookie(*args, **kwargs):
+        return super().set_cookie(*args, secure=True, httponly=True, **kwargs)
+
+    def set_default_headers(self):
+        self.add_header('X-Frame-Options', 'SAMEORIGIN')
+        self.add_header('X-XSS-Protection', '1; mode=block')
+        self.add_header('X-Content-Type-Options', 'nosniff')
+        # self.add_header(
+        #     'Strict-Transport-Security',
+        #     'max-age=31536000; includeSubDomains'
+        # )
+
+
+class JSONBaseHandler(BaseHandler):
+    render = BaseHandler.write
--- app/main.py
+++ app/main.py
@@ -2,18 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import os
-import binascii
 import pathlib
 
 import tornado.ioloop
 import tornado.web
-from tornado import gen
-from tornado.escape import to_unicode
 from tornado.options import parse_command_line
 
 
 class TornadoApp(tornado.web.Application):
-    def __init__(self, env):
+    def __init__(self, env, cookie_secret):
         debug = True
         traceback = True
 
@@ -28,29 +25,21 @@ class TornadoApp(tornado.web.Application):
         settings = {
             'template_path': template_path,
             'static_path': static_path,
-            'cookie_secret': to_unicode(binascii.hexlify(os.urandom(64))),
+            'cookie_secret': cookie_secret,
             'xsrf_cookies': True,
             'debug': debug,
             'serve_traceback': traceback
         }
 
         handlers = [
-            (r'/', MainHandler),
+            # (r'/', MainHandler),
         ]
         super().__init__(handlers, **settings)
 
 
-class MainHandler(tornado.web.RequestHandler):
-    @gen.coroutine
-    def get(self, page="1"):
-        self.render("index.html")
-
-    def render(self, *args, **kwargs):
-        self.add_header('X-Content-Type-Options', 'nosniff')
-        super().render(*args, **kwargs)
-
 if __name__ == '__main__':
     parse_command_line()
     env = os.environ['TORNADO_ENV']
+    cookie_secret = os.environ['TORNADO_COOKIE_SECRET']
     TornadoApp(env).listen(int(os.environ['PORT']))
     tornado.ioloop.IOLoop.instance().start()
