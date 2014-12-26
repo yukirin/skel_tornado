b6c059333bca6669cf675b8cb13c6032e9537799
yukirin <standupdown@gmail.com>
2014-12-27 07:25:44 +0900

    Add assertRaises

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,6 +9,7 @@ from test.support import EnvironmentVarGuard
 sys.path[0:0] = [str(pathlib.Path(__file__).parent.resolve() / '..' / 'app')]
 
 from tornado.testing import AsyncHTTPTestCase, gen_test
+from tornado.httpclient import HTTPError
 
 from main import TornadoApp
 from foremanenvparser import ForemanEnvParser
@@ -26,3 +27,9 @@ class TornadoAppTest(AsyncHTTPTestCase):
     def test_app(self):
         response = yield self.http_client.fetch(self.get_url('/'))
         self.assertEqual(str(response.body, 'utf-8'), "Hello, world")
+
+    @gen_test
+    def test_404(self):
+        with self.assertRaises(HTTPError) as cm:
+            yield self.http_client.fetch(self.get_url('/hogehoge'))
+        self.assertEqual(cm.exception.code, 404)
