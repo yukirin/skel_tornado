4954a9eee8cc30f93c320a747a848364374ea219
yukirin <standupdown@gmail.com>
2014-12-28 02:08:01 +0900

    Add js template

--- app/development/static/js/app.js
+++ app/development/static/js/app.js
@@ -1,1 +1,27 @@
 
+(function(global) {
+"use strict";
+
+// --- dependency modules ----------------------------------
+// --- define / local variables ----------------------------
+var _runOnNode = "process" in global;
+var _runOnWorker = "WorkerLocation" in global;
+var _runOnBrowser = "document" in global;
+
+// --- class / interfaces ----------------------------------
+function MyClass() {
+}
+
+MyClass["prototype"] = {
+  "method": MyClass_method
+};
+
+// --- implements ------------------------------------------
+function MyClass_method(name) { 
+}
+
+// --- exports ---------------------------------------------
+module["exports"] = MyClass;
+global["MyClass" in global ? "MyClass_" : "MyClass"] = MyClass;
+
+})((this || 0).self || global);
